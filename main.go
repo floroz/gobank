d4909bfbab1539853566e1ae5fa5637a21ef59ab
Daniele Tortora <daniele.tortora@snyk.io>
2024-10-26 21:42:22 +0200

    feat: load env file

--- main.go
+++ main.go
@@ -2,9 +2,12 @@ package main
 
 import (
 	"github.com/floroz/gobank/api"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	godotenv.Load()
+
 	server := api.NewAPIServer("localhost:8080")
 	server.Run()
 }
