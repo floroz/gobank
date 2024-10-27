160e0fffbb8a2163a3992525d336ba1b35d32183
Daniele Tortora <daniele.tortora@snyk.io>
2024-10-27 19:19:08 +0100

    refactor: extract logic from api server

--- api/api.go
+++ api/api.go
@@ -1,11 +1,8 @@
 package api
 
 import (
-	"database/sql"
-	"fmt"
 	"log"
 	"net/http"
-	"os"
 
 	"github.com/floroz/gobank/api/middlewares"
 	"github.com/floroz/gobank/api/resources/account"
@@ -15,38 +12,19 @@ import (
 
 type APIServer struct {
 	listenAddress string
+	storage       storage.Storager
 }
 
-func NewAPIServer(listenAddress string) *APIServer {
+func NewAPIServer(listenAddress string, storage storage.Storager) *APIServer {
 	return &APIServer{
 		listenAddress: listenAddress,
+		storage:       storage,
 	}
 }
 
 func (s *APIServer) Run() {
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	host := os.Getenv("DB_HOST")
-	port := os.Getenv("DB_PORT")
-	dbName := os.Getenv("DB_NAME")
-
-	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, host, port, dbName)
-
-	db, err := sql.Open("postgres", dsn)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	err = db.Ping()
-	if err != nil {
-		// do something here
-	}
-
 	mux := http.NewServeMux()
-	storage := storage.NewPostgreSQLStorage()
-	accountRouter := account.NewAccountRouter(storage)
+	accountRouter := account.NewAccountRouter(s.storage)
 
 	mux.HandleFunc("GET /account/{id}", accountRouter.GetAccount)
 	mux.HandleFunc("POST /account", accountRouter.CreateAccount)
--- main.go
+++ main.go
@@ -1,13 +1,53 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
+	"os"
+
 	"github.com/floroz/gobank/api"
+	"github.com/floroz/gobank/storage"
 	"github.com/joho/godotenv"
 )
 
-func main() {
-	godotenv.Load()
+func genDbConnectionString() string {
+	user := os.Getenv("DB_USER")
+	password := os.Getenv("DB_PASSWORD")
+	host := os.Getenv("DB_HOST")
+	port := os.Getenv("DB_PORT")
+	dbName := os.Getenv("DB_NAME")
 
-	server := api.NewAPIServer("localhost:8080")
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%s)/%s", user, password, host, port, dbName)
+
+	return dsn
+}
+
+func loadEnv() {
+	godotenv.Load()
+}
+
+func connectDb() {
+	db, err := sql.Open("postgres", genDbConnectionString())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil {
+		// do something here
+	}
+}
+
+func main() {
+	loadEnv()
+	connectDb()
+	storage := storage.NewPostgreSQLStorage()
+
+	hostname := fmt.Sprintf("%s:%s", os.Getenv("HOST"), os.Getenv("PORT"))
+
+	server := api.NewAPIServer(hostname, storage)
 	server.Run()
 }
